22ff30cd1abf660d3d48de5529d38878b2d5dd45
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-09-11 04:17:26 +0000

    Automatically backed up by Learn

--- db/migrate/20190911041557_create_doctors.rb
+++ db/migrate/20190911041557_create_doctors.rb
@@ -1,8 +1,10 @@
-class CreateDoctors < ActiveRecord::Migration[5.0]
+class CreateDoctors < ActiveRecord::Migration
   def change
     create_table :doctors do |t|
+      t.string :department
+      t.string :name
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
